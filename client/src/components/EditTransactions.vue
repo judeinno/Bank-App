2648145f30cd54488acd8bac61c26cfa4fb46a18
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-11 14:41:57 +0300

    Add vue file

---
+++ client/src/components/EditTransactions.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Edit transactions</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <transctions></transctions>
+    <edit-transactions></edit-transactions>
+  </div>
+</template>
+
+<script>
+import Transctions from './Transactions.vue'
+import EditTransactions from './EditTransactions.vue'
+
+export default {
+  components: {
+    Transctions,
+    EditTransactions
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
